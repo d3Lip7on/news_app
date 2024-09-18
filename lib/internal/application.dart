249d9ba0bde7ab1370d64ad21615e15f3b4158c2
Vladislav Vagner <vladislawwagner@gmail.com>
2024-09-18 14:07:46 +0200

    second commit

---
+++ lib/internal/application.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class NewsApp extends StatelessWidget {
+  const NewsApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: HomePage());
+  }
+}
